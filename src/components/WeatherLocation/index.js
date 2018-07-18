c7329d6818b30caf6c3b97b695262cdea4717d1c
Manuel Gonzalez <manuelarvelo@hotmail.com>
2018-07-18 17:42:44 +0200

    uso de componente funcionales, PropTypes y configuracion api weather

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
+import {  SUN } from './../../constants/weathers';
 
-const WeatherLocation = () => (
-    <div className='weatherLocationContainer' >
-        <Location city={'Buenos Aires!'}/>
-        <WeatherData/>
-    </div>
-);
+const location = 'Buenos Aires, ar';
+const key = 'e49083fd2506c38974d99a7f26824d4c';
+const api_weather = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
+const data = {
+     temperature: 20,   
+     weatherState: SUN,
+     humidity: 10,
+     wind: '10 m/s'
+}
+
+class  WeatherLocation extends Component{  //class component: así puedo trabajar con el lifecycle, Y EL ESTADO.
+    constructor(){
+        super();
+        this.state = {
+            data: data,
+            city: 'Buenos Aires!'
+        }
+    }
+    handleUpdateClick = () =>{
+        
+        fetch(api_weather).then((data)=>{
+            console.log(data)
+        })
+        // this.setState({
+        //     city: 'Santo Domingo!',
+        //     data: data2
+        // })
+    }
+    render = () =>{
+        
+        const { city, data } = this.state;
+        return (
+            < div className='weatherLocationContainer' >
+                <Location city={city}/>
+                <WeatherData data={data}/>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
+            </div>
+        )}
+};
 
 export default WeatherLocation;
 
