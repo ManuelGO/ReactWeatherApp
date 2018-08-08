5b690a4cbd83604bbc1a4e2eeeba2589a9340acf
Manuel Gonzalez <manuelarvelo@hotmail.com>
2018-08-08 16:07:09 +0200

    location list container creado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import LocationList from './components/LocationList';
+import LocationListContainer from './containers/LocationListContainer';
 import {Grid, Row, Col} from 'react-flexbox-grid'
-import PropTypes from 'prop-types';
 import './App.css';
 import ForecastExtended from './components/ForecastExtended';
-import {setCity} from './actions';
 //import { MuiThemeProvider} from 'material-ui/styles/MuiThemeProvider'
 
 const cities = [
@@ -24,10 +21,7 @@ class App extends Component {
     super() //esto llama el constructor de Component
     this.state =  {city: null};  //esta asignación sólo se puede hacer en el constructor, a partir de ahí setState.
   }
-  handleSelectionLocation = city => {
-    this.setState({ city });
-    this.props.dispatchSetCity(city);
-  }
+
 
   render() {
     const {city} = this.state;
@@ -40,9 +34,8 @@ class App extends Component {
           </Row>
           <Row>
             <Col xs={12} md={6}>
-              <LocationList cities={cities} 
-                      onSelectedLocation={this.handleSelectionLocation}>
-              </LocationList>
+              <LocationListContainer cities={cities}>
+              </LocationListContainer>
             </Col>
             <Col  xs={12} md={6}>
               <div className='detail'>
@@ -59,16 +52,7 @@ class App extends Component {
   }
 }
 
-App.propTypes = {
-  dispatchSetCity: PropTypes.func.isRequired,
-}
-
-const mapDispatchToPropsActions = dispatch => ({
-  dispatchSetCity: value => dispatch(setCity(value))
-});
 
 
 
-const AppConnected = connect(null, mapDispatchToPropsActions)(App);
-
-export default AppConnected;
+export default App;
