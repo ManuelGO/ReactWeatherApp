2712e76e32bf623d3afaf39f34ff666da3d32e95
Manuel Gonzalez <manuelarvelo@hotmail.com>
2018-08-09 16:53:42 +0200

    uso de redux-thunk middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,30 @@
+import transformForecast from '../services/transformForecast';
 
 export const SET_CITY = 'SET_CITY';
+export const SET_FORECAST_DATA = 'SET_FORECAST_DATA';
 
-export const setCity = payload => ({type: SET_CITY, payload }); //action creator
+const setCity = payload => ({type: SET_CITY, payload }); //action creator
+const setForecastData = payload => ({type: SET_FORECAST_DATA, payload});
+const key = 'e49083fd2506c38974d99a7f26824d4c';
+const url = 'https://api.openweathermap.org/data/2.5/forecast';
+
+export const setSelectedCity = payload => {
+    return dispatch => {  //middleware thunk
+        const url_forecast = `${url}?q=${payload}&appid=${key}&units=metric`;
+
+        //activar en el estado un indicador de busqueda de datos:
+        dispatch(setCity(payload));
+
+        return fetch(url_forecast).then(
+                data => data.json()
+                ).then(
+                    weather_data => {
+                        const forecastData = transformForecast(weather_data);
+
+                        //modificar el estado con el resultado de la promise(fetch).
+                        dispatch(setForecastData({city: payload, forecastData}));
+                    }
+                );
+
+    }
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,21 @@
-import { createStore} from 'redux';
+import { createStore , applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 import  {city} from './../reducers/city';
 
 const initialState = {
     city: 'Buenos Aires'
 };
 
-export const store = createStore(city, initialState,
-                window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(
+        city, 
+        initialState,
+        compose(
+            applyMiddleware(thunk,),
+            window.devToolsExtension ? window.devToolsExtension() : f => f
+          )
+        );
+
+
+//sin middleware
+// export const store = createStore(city, initialState,
+//                 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
